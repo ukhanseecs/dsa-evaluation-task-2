48ce066335b40d876f998b74ff13726f59976d9e
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2025-02-15 14:46:04 +0500

    logic improv

--- wave_pattern.go
+++ wave_pattern.go
@@ -1,6 +1,8 @@
 package main
 
-
+import (
+	"sort"
+)
 
 // Function to rearrange array in wave pattern
 func waveRearrange(arr []int, x int) {
@@ -13,45 +15,24 @@ func waveRearrange(arr []int, x int) {
 		if end > n {
 			end = n
 		}
-		processBlock(arr[i:end], x)
+		processBlock(arr, i, end, x)
 	}
 }
 
-// Process a single block of size (2x+1)
-func processBlock(block []int, x int) {
-	size := len(block)
+// Process a single block of size (2x+1) in-place
+func processBlock(arr []int, start, end, x int) {
+	size := end - start
 	if size <= 1 {
 		return
 	}
 
-	// Sort left part (0 to x) in ascending order
-	for i := 0; i < x; i++ {
-		minIndex := i
-		for j := i + 1; j <= x && j < size; j++ {
-			if block[j] < block[minIndex] {
-				minIndex = j
-			}
-		}
-		block[i], block[minIndex] = block[minIndex], block[i]
-	}
+	// Sort the block
+	sort.Slice(arr[start:end], func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
 
-	// Find max element in the block and place it at index x
-	maxIndex := x
-	for i := x; i < size; i++ {
-		if block[i] > block[maxIndex] {
-			maxIndex = i
-		}
-	}
-	block[x], block[maxIndex] = block[maxIndex], block[x]
-
-	// Sort right part (x+1 to size-1) in descending order
-	for i := x + 1; i < size; i++ {
-		maxIndex := i
-		for j := i + 1; j < size; j++ {
-			if block[j] > block[maxIndex] {
-				maxIndex = j
-			}
-		}
-		block[i], block[maxIndex] = block[maxIndex], block[i]
-	}
+	// Swap the middle element with the last element
+	mid := start + x
+	maxIdx := end - 1
+	arr[mid], arr[maxIdx] = arr[maxIdx], arr[mid]
 }
